6eeab7528a05aacbadb21b0246f0d8ce1fc0672e
adelakim5 <diddnjs02@likelion.org>
2019-07-30 19:38:57 +0900

    first commit

--- yangwonkimproject/settings.py
+++ yangwonkimproject/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')g_(*9be(2r@*m^ubrdut!w@c-rt)3-4!id=-8xgfyv^+5z6@('
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')g_(*9be(2r@*m^ubrdut!w@c-rt)3-4!id=-8xgfyv^+5z6@(')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
